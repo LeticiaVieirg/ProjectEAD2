029cdd625ac9a05f3d27268b5470c7ce3f48a56d
Allan Freitas <allangabrieldev@gmail.com>
2024-08-30 09:57:57 -0300

    feat: declare stock management functions in header file

--- include/stock.h
+++ include/stock.h
@@ -1,10 +1,10 @@
+//declaring the libraries
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 // Defining hash table size
 #define SIZE 13
-// 599
-
+//Declaration of the input type structure
 typedef struct {
     int amount;
     int barcode;
@@ -16,5 +16,24 @@ typedef struct node{
     Inputs input;
     struct node *next;
 } Node;
-
 typedef Node* List;
+
+//fuctions
+//Function that initializes the hash table
+void initializeTable(List table[]);
+//function that generates my hash
+int hashFunction(int key);
+//Function that inserts the inputs into the hash table
+void insertInputs(List table[]);
+//Function searches for a specific element in the table
+Node *search(List table[], int key);
+//Function prints the hash table
+void printInputs(List table[]);
+//Function prints the hash table in the text file
+void printFileTxt(List table[]);
+//Function that reads data from the text file and throws it into the hash table
+List *loadFromFile(List table[]);
+//Function that reduces the amount of inputs
+void decrement(List table[]);
+//Function that increases the amount of inputs
+void increment(List table[]);
