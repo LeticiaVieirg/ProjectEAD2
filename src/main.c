b5452bc91f40becd1cab543b695a6929b7af03ab
Letícia Vieira Gonçalves <90807534+LeticiaVieirg@users.noreply.github.com>
2024-09-06 17:00:06 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,1 +1,45 @@
+int main(void) {
+    int option, serachKey;
+    Inputs table[SIZE];
+    Inputs *foundItem;
 
+    initializeTable(table);
+
+    do {
+        printf("\n\t0 - Sair\n\t1 - Inserir\n\t2 - Buscar\n\t3 - Imprimir\n");
+        scanf("%d", &option);
+        getchar();
+
+        switch (option) {
+            case 1:
+                insertInputs(table);
+                break;
+            case 2:
+                printf("Enter barcode to search: ");
+                scanf("%d", &serachKey);
+                getchar();
+
+                foundItem = search(table, serachKey);
+                if(foundItem != NULL){
+                    printf("Product found: \n");
+                    printf("\tBarcode: %d\n", foundItem->barcode);
+                    printf("\tProduct: %s\n", foundItem->productName);
+                    printf("\tAmount: %d\n", foundItem->amount);
+                } else{
+                    printf("Product not found.\n");
+                }
+                break;
+            case 3:
+                printInputs(table);
+                break;
+            case 0:
+                printf("Exiting...\n");
+                break;
+            default:
+                printf("Opção informada é inválida...\n");
+        }
+
+    } while (option != 0);
+
+    return 0;
+}
