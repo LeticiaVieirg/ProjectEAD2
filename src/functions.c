c0b87d24673a6eacff43cafc18265f917a0ef799
Isabelly Lima <mslima5643@gmail.com>
2024-09-10 09:27:22 -0300

    tirar do menu remover insumo

--- src/functions.c
+++ src/functions.c
@@ -7,6 +7,5 @@ printf("[1] Adicionar consulta===");
 printf("[2] Buscar Consulta\n");
 printf("[3] Remover Consulta\n");
 printf("[4] Adicionar insumo ao estoque\n");
-printf("[5] Remover insumo\n");
-printf("[6] Buscar Insumo no estoque\n");
-printf("[7] Buscar Paciente\n"); }
+printf("[5] Buscar Insumo no estoque\n");
+printf("[6] Buscar Paciente\n"); }
