079a1e7e2ca87dfc6b61858c93b0e0bb39dff42d
Isabelly Lima <mslima5643@gmail.com>
2024-09-10 09:25:46 -0300

    colocando a funcao do menu

---
+++ src/functions.c
@@ -0,0 +1,12 @@
+#include "../include/functions.h"
+
+
+void menu(){
+printf("=========MENU===========");
+printf("[1] Adicionar consulta===");
+printf("[2] Buscar Consulta\n");
+printf("[3] Remover Consulta\n");
+printf("[4] Adicionar insumo ao estoque\n");
+printf("[5] Remover insumo\n");
+printf("[6] Buscar Insumo no estoque\n");
+printf("[7] Buscar Paciente\n"); }
