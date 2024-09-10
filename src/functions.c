a9674d173a882a162b705c1956b2193325c8e565
Isabelly Lima <mslima5643@gmail.com>
2024-09-10 09:28:10 -0300

    pequenas correcoes

--- src/functions.c
+++ src/functions.c
@@ -3,7 +3,7 @@
 
 void menu(){
 printf("=========MENU===========");
-printf("[1] Adicionar consulta===");
+printf("[1] Adicionar consulta\n");
 printf("[2] Buscar Consulta\n");
 printf("[3] Remover Consulta\n");
 printf("[4] Adicionar insumo ao estoque\n");
