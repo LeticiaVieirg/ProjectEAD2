eb85437cce407e0cb8980b2fe3b988db26ba5944
Letícia Vieira Gonçalves <90807534+LeticiaVieirg@users.noreply.github.com>
2024-09-14 18:43:41 -0400

    Update patient.h
    fix: declarate the functions of patient.c

--- include/patient.h
+++ include/patient.h
@@ -1,11 +1,21 @@
+//Declaring the librarers
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+//Defining size
 #define MAX_PATIENTS 100 
 
+//Declarate of the input type structure 
 typedef struct {
     char name[50]; 
     char cpf[15];  
     int age;
 } Patient;
+
+//Functions
+//Function that find patient by CPF
+int find_patient_by_cpf(char *cpf);
+
+//Function that register patient
+void register_patient();
