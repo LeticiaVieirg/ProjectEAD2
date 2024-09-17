408dd1450a23ba3bf6a543d22596246493400252
Allan Freitas <allangabrieldev@gmail.com>
2024-09-17 09:43:49 -0300

    feat: I will add the functions insertIntoHeap and heapBelow

--- src/consultation.c
+++ src/consultation.c
@@ -23,3 +23,46 @@ void heapUp(Heap *heap, int index){
         heapUp(heap, parent);
     }
 }
+
+void insertIntoHeap(Heap *heap){
+    if(heap->size == heap->capacity){
+        printf("Full heap!\n");
+        return;
+    }
+
+    Patient newPatient;
+    List table[SIZE];
+
+    printf("Enter the patient's age: ");
+    scanf("%d", &newPatient.age);
+
+    printf("Inform the procedure to be carried out: ");
+    getchar();
+    fgets(newPatient.description, 50, stdin);
+    newPatient.description[strcspn(newPatient.description, "\n")] = '\0';
+
+    decrement(table);
+
+    heap->Patient[heap->size] = newPatient;
+    heapUp(heap, heap->size);
+    heap->size++;
+}
+
+void heapBelow(Heap *heap, int index){
+    int bigger = index;
+    int left = 2 * index + 1;
+    int right = 2 * index + 2;
+
+    if(left < heap->size && heap->Patient[left].age > heap->Patient[bigger].age){
+        bigger = left;
+    }
+
+    if(right < heap->size && heap->Patient[right].age > heap->Patient[bigger].age){
+        bigger = right;
+    }
+
+    if(bigger != index){
+        swap(&heap->Patient[index], &heap->Patient[bigger]);
+        heapBelow(heap, bigger);
+    }
+}
