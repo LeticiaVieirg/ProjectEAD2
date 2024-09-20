ada06b1e7de21d1b231c0a0fe42dc3215202987a
Letícia Vieira Gonçalves <90807534+LeticiaVieirg@users.noreply.github.com>
2024-09-20 07:49:10 -0400

    Update consultation.h
    fix: Added the description in the functions

--- include/consultation.h
+++ include/consultation.h
@@ -2,13 +2,40 @@
 #include "../include/stock.h"
 #include "../include/patient.h"
 
+//
 typedef struct {
     int age;
     char description[50];
 } Patient;
 
+//
 typedef struct {
     Patient *Patient;
     int size;
     int capacity;
 } Heap;
+
+//Functions
+//Creates a new heap with the specified capacity, initializing the patient array, size, and capacity
+Heap *createHeap(int capacity);
+
+//Exchanges the contents of two patient structures
+void swap(Patient *patientOne, Patient *patientTwo);
+
+//Compares the element at the given index with its parent and swaps them
+void heapUp(Heap *heap, int index);
+
+//Prompts the user for patient information, creates a new patient object, inserts it into the heap, and calls heapUp
+void insertIntoHeap(Heap *heap);
+
+//Compares the element at the given index with its children and swaps it with the larger child
+void heapBelow(Heap *heap, int index);
+
+//Replaces the root with the last element, removes the last element, and calls heapBelow
+Patient removeHeap(Heap *heap);
+
+//Fries the memory heap allocated for the patient array and the heap structure itself
+void freeHeap(Heap *heap) {
+
+//Print the patient information for each elements in the heap
+void displayHeap(Heap *heap) {
